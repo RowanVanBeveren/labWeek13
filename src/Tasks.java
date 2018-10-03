06dbb371ee02c6ce4aa49a86e7bdfc77b76fc223
RowanVanBeveren <s5107347@bournemouth.ac.uk>
2018-10-03 11:56:15 +0100

    please go to the right repository

---
+++ src/Tasks.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Tasks {
+
+    public static void main(String[] args) {
+
+    	Scanner input = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    public static void playingWithStrings(){
+			String name = "Melanie";
+			String myName = "Rowan van Beveren";
+
+			//Task 1
+			System.out.println(name);
+			System.out.println(name.toUpperCase());
+
+			//Task 2
+			System.out.println(myName.length());
+
+			//Task 3
+			System.out.println(myName.substring(2, 5));
+
+		}
+
+	public static void playingWithMaths() {
+		double num = 15.7;
+		int random;
+
+		System.out.println(Math.ceil(num));
+		System.out.println((int) num);
+		random = (int) (Math.random() * 10 + 1);
+		System.out.println(random);
+	}
+
+}
